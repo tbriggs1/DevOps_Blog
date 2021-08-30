ab321e1ceb1047fa1bfa44bd02e8adcd04aa31f8
tbriggs1 <tombriggs93@protonmail.com>
2021-08-30 20:11:16 +0100

    added databse code

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Blogs(models.Model):
+    title = models.Charfield(max_length=200)
+    description = models.Charfield(max_length=20000)
+    image = models.Charfield(max_length=2000)
+
+
+class User(models.Model):
+    pass
