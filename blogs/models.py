cde3946ca70c8306ac1589ec2c846d6b6162a6bb
tbriggs1 <tombriggs93@protonmail.com>
2021-08-30 20:31:58 +0100

    fixed char error

--- blogs/models.py
+++ blogs/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Blogs(models.Model):
-    title = models.Charfield(max_length=200)
-    description = models.Charfield(max_length=20000)
-    image = models.Charfield(max_length=2000)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=20000)
+    image = models.CharField(max_length=2000)
 
 
 class User(models.Model):
